9bc5f0782d7c54dc310ddb1edf3acf7b7a91de28
akpsan <anandpcaj@gmail.com>
2022-01-17 08:20:48 +0530

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [products, setProducts] = useState(null)
+  const [symbolPriceMap, setSymbolPriceMap] = useState({})
+
+  useEffect(() => {
+    axios.get('https://api.delta.exchange/v2/products', {
+      params: {
+        page_size: 200
+      }
+    }).then(res => {
+      setProducts(res.data)
+      const socket = new WebSocket('wss://production-esocket.delta.exchange');
+      socket.onopen = function () {
+        // Get markprice
+        const symbolList = res.data.result.map(prod => prod.symbol)
+        symbolList.forEach(symbol => symbolPriceMap[symbol] = null)
+        socket.send(JSON.stringify({
+          "type": "subscribe",
+          "payload": {
+            "channels": [
+              {
+                "name": "v2/ticker",
+                "symbols": [
+                  ...symbolList
+                ]
+              }
+            ]
+          }
+        }))
+      }
+
+      socket.onmessage = function (msg) {
+        const parsedData = JSON.parse(msg.data)
+        if (parsedData.symbol) {
+          let newData = {}
+          newData[parsedData.symbol] = parsedData.mark_price
+          setSymbolPriceMap(prev => ({
+            ...prev,
+            ...newData
+          }))
+        }
+      }
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='table-wrapper'>
+        <table className='table'>
+          <thead className='table-header'>
+            <tr>
+              <th>Symbol</th>
+              <th>Description</th>
+              <th>Underlying Asset</th>
+              <th>Mark Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products && products.result.map(prod =>
+            (<tr>
+              <td>{prod.symbol}</td>
+              <td>{prod.description}</td>
+              <td>{prod.underlying_asset.symbol}</td>
+              <td>{symbolPriceMap[prod.symbol]}</td>
+            </tr>)
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
